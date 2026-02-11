f115db5b4b7df9628f220bfe6a125e384f61a007
lourealiza <lou@realizati.com.br>
2026-02-11 00:17:12 -0300

    chore(debug): add logging to consultaRoutes to trace undefined export

--- backend/src/routes/consultaRoutes.js
+++ backend/src/routes/consultaRoutes.js
@@ -11,6 +11,24 @@ const { validarConsulta, tratarErrosValidacao } = require('../middleware/validac
  * Aceita tanto consultas simples quanto reservas completas
  * Campos nome_hospede, email_hospede e telefone_hospede são opcionais
  */
+console.log('DEBUG: Loading consultaRoutes...');
+console.log('DEBUG: validarConsulta:', typeof validarConsulta);
+console.log('DEBUG: tratarErrosValidacao:', typeof tratarErrosValidacao);
+console.log('DEBUG: reservaController:', typeof reservaController);
+if (reservaController) {
+    console.log('DEBUG: reservaController.consultar:', typeof reservaController.consultar);
+}
+
+// Debug route to check router status
+router.get('/debug-check', (req, res) => {
+    res.json({
+        status: 'ok',
+        validarConsultaType: typeof validarConsulta,
+        tratarErrosValidacaoType: typeof tratarErrosValidacao,
+        consultarType: typeof reservaController?.consultar
+    });
+});
+
 router.post('/', validarConsulta, tratarErrosValidacao, reservaController.consultar);
 
 module.exports = router;
